5add5d98f78a3a4bb034feeea06c40edde4e8e15
rodionbogoveev <rodiononon@yandex.ru>
2022-01-06 00:20:51 +0300

    пишу модели

--- reformagkh_parser/site_parser/parse.py
+++ reformagkh_parser/site_parser/parse.py
@@ -1,8 +1,8 @@
+from urllib.parse import quote
+
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-from urllib.parse import quote
-
 
 URL = 'https://www.reformagkh.ru'
 HEADERS = {'user-agent': UserAgent().random, 'accept': '*/*'}
@@ -25,21 +25,32 @@ def get_links(html):
 def get_gen_info(html):
     soup = BeautifulSoup(html, 'html.parser')
     items = soup.find('div', class_='col-9 tab-content')
-    # with open('page.html', 'w') as file:
-    #     file.write(items.prettify())
-    year = items.find(string='Год ввода дома в эксплуатацию').find_parent('td').find_next().text.strip()
-    floors = items.find(string='Количество этажей, ед.').find_parent('td').find_next().text.strip()
-    updating = items.find(string='По данным Фонда ЖКХ информация последний раз актуализировалась:').find_parent('td').find_next().text.strip()
-    series = items.find(string='Серия, тип постройки здания').find_parent('td').find_next().text.strip()
-    type_of_building = items.find(string='Тип дома').find_parent('td').find_next_siblings()[-1].text.strip()
+    year = items.find(string='Год ввода дома в эксплуатацию')
+    year = year.find_parent('td').find_next().text.strip()
+    floors = items.find(string='Количество этажей, ед.')
+    floors = floors.find_parent('td').find_next().text.strip()
+    updating = items.find(
+        string='По данным Фонда ЖКХ информация последний раз '
+               'актуализировалась:'
+    )
+    updating = updating.find_parent('td').find_next().text.strip()
+    series = items.find(string='Серия, тип постройки здания')
+    series = series.find_parent('td').find_next().text.strip()
+    type_of_building = items.find(string='Тип дома')
+    type_of_building = type_of_building.find_parent('td').find_next_siblings()
+    type_of_building = type_of_building[-1].text.strip()
     emergency = items.find(string='Факт признания дома аварийным')
-    if emergency is not None:
-        emergency = emergency.find_parent('td').find_next_siblings()[-1].text.strip()
+    if emergency:
+        emergency = emergency.find_parent('td')
+        emergency = emergency.find_next_siblings()[-1].text.strip()
     cadastre = items.find(string='Кадастровый номер земельного участка')
-    if cadastre is not None:
-        cadastre = cadastre.find_parent('td').find_next_siblings()[-1].text.strip()
-    floor = items.find(string='Стены и перекрытия. Тип перекрытий').find_parent('td').find_next_siblings()[-1].text.strip()
-    walls = items.find(string='Стены и перекрытия. Материал несущих стен').find_parent('td').find_next_siblings()[-1].text.strip()
+    if cadastre:
+        cadastre = cadastre.find_parent('td').find_next_siblings()
+        cadastre = cadastre[-1].text.strip()
+    floor = items.find(string='Стены и перекрытия. Тип перекрытий')
+    floor = floor.find_parent('td').find_next_siblings()[-1].text.strip()
+    walls = items.find(string='Стены и перекрытия. Материал несущих стен')
+    walls = walls.find_parent('td').find_next_siblings()[-1].text.strip()
     data = {
         'year': year,
         'floors': floors,
@@ -75,12 +86,13 @@ def parse(query):
         html_gen_info = get_html(URL + link)
         if html_gen_info.status_code == 200:
             data = get_gen_info(html_gen_info.text)
-            with open('result.txt', 'a') as file:
+            with open(r'result.txt', 'a') as file:
                 file.write(f'{data}\n')
         else:
             print('Сайт не отвечает')
     else:
         print('Не удалось получить данные о доме.')
+
 
 query = [
     'Санкт-Петербург линия 6-я В.О. д 47',
--- reformagkh_parser/site_parser/read_excel.py
+++ reformagkh_parser/site_parser/read_excel.py
@@ -1,30 +1,15 @@
 import openpyxl
 
 
-A = 'г. Санкт-Петербург, ул. Репищева, д. 21, к. 1'
-
-
 def open_excel():
-    data = openpyxl.open('test_data.xlsx', read_only=True)
+    data = openpyxl.open(r'test_data.xlsx', read_only=True)
     sheet = data.active
     for row in range(2, sheet.max_row + 1):
-    # for row in range(2, 10):
         row = sheet[row]
         region = row[2].value.split()
-        # if region[1].lower() == 'г':
-        #     region_type = 'г.'
-        # elif 'респ' in region[1].lower():
-        #     region_type = 'Респ.'
-        # elif 'обл' in region[1].lower():
-        #     region_type = 'обл.'
-        # elif 'край' in region[1].lower():
-        #     region_type = 'край.'
-        # region = f'{region_type} {region[0]}'
         region = region[0]
         settlement = row[4].value
         settlement_type = row[3].value
-        # if settlement_type:
-        #     settlement_type += '.'
         if not settlement:
             settlement = None
         else:
@@ -35,13 +20,13 @@ def open_excel():
             street = None
         else:
             street = f'{street_type} {street}'
-        # street = street + street_type
         house = f'д {row[7].value}'
         block = row[8].value
         if not block:
             block = None
         else:
             block = f'к {block}'
+        address = f'{region} {settlement} {street} {house} {block}'
         query = {
             'region': region,
             'settlement': settlement,
@@ -49,9 +34,14 @@ def open_excel():
             'house': house,
             'block': block,
         }
+        address = ''
         for i in query:
             if query[i]:
-                print(query[i], end=' ')
-        print()
+                address += f'{query[i]} '
+        query['address'] = address
+
+        with open('queryset.txt', 'a') as file:
+            file.write(f'{query["address"]}\n')
+
 
 open_excel()
